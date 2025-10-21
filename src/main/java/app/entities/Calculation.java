3220d38c0d93f9c1be1ffb002b0fe408786b7161
MarcusPFF <marcus.pff03@gmail.com>
2025-10-21 16:56:09 +0200

    Database & Entities test with HibernateConfigTest

---
+++ src/main/java/app/entities/Calculation.java
@@ -0,0 +1,37 @@
+package app.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "calculations")
+public class Calculation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private double num1;
+
+    @Column(nullable = false)
+    private double num2;
+
+    @Column(nullable = false)
+    private double result;
+
+    @Column(nullable = false)
+    private String operation;
+
+    @Column(nullable = false)
+    private LocalDateTime timestamp;
+
+    // Relationship to User
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+}
